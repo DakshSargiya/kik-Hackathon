dcf1496675ed56a7d4998e2e2890fbc57e3d8c8a
Tanush <tanush@myyahoo.com>
2024-03-02 04:36:30 +0530

    cors

--- main.py
+++ main.py
@@ -2,10 +2,17 @@ import json
 from flask import Flask
 from flask import request
 from mainProcess import getOptions
+from flask_cors import CORS, cross_origin
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 __name__ = "main"
 app = Flask(__name__)
 
+
 @app.route("/", methods=['GET', 'POST'])
+@cross_origin()
 def hello_world():
     if request.method == 'POST':
         data = request.json
